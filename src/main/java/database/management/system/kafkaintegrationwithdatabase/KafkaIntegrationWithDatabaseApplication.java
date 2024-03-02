a9b3ed593ef84eee3b9f43507ab267bd966767e2
Santrosyan03 <sevaksantrosyan5@gmail.com>
2024-03-02 23:30:05 +0400

    first commit

---
+++ src/main/java/database/management/system/kafkaintegrationwithdatabase/KafkaIntegrationWithDatabaseApplication.java
@@ -0,0 +1,13 @@
+package database.management.system.kafkaintegrationwithdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaIntegrationWithDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaIntegrationWithDatabaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/database/management/system/kafkaintegrationwithdatabase/KafkaIntegrationWithDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package database.management.system.kafkaintegrationwithdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaIntegrationWithDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
